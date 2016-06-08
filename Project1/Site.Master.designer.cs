50345e4935e2e412881caee69b8ba9216f293862
mack0082 <mack.waters0803@gmail.com>
2016-06-08 00:31:44 -0400

    deafault page added

--- Project1/Site.Master.designer.cs
+++ Project1/Site.Master.designer.cs
@@ -40,12 +40,12 @@ namespace Project1 {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl home;
         
         /// <summary>
-        /// ContentPlaceHolder1 control.
+        /// MainContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
